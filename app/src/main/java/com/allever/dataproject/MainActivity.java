c0ea35a3afa90aa1815a18c2d66e031e9fd8a9c8
devallever <devallever@163.com>
2020-10-31 11:27:07 +0800

    提交代码

--- app/src/main/java/com/allever/dataproject/MainActivity.java
+++ app/src/main/java/com/allever/dataproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.allever.dataproject;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -8,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity();
     }
 }
